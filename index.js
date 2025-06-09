b5f6bb5e342cfe8208756cd695ef7ba20029916f
fiyosa <fiyosa97@gmail.com>
2025-06-09 17:49:12 +0700

    new

--- index.js
+++ index.js
@@ -19,7 +19,8 @@ require('dotenv').config();
 app.use(cors({
   // origin: ['http://localhost:3000', 'http://localhost:5173'],
   origin: '*',
-  credentials: true
+  // credentials: true
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
 }));
 
 app.use(express.json());
